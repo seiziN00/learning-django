651e8efc5d125ba779e754fd37ea1528cd82c226
Luis F. Tello A. <seizin00@tuta.io>
2026-04-16 11:56:05 -0500

    Modular URL Routing: 'comments' app

--- modularization/comments/views.py
+++ modularization/comments/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def test(request):
+  return HttpResponse("It works!")
+
+def create(request):
+  return HttpResponse("Create a comment")
