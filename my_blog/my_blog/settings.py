b11f91b58c52a844a74a2d7a29f800252e27682d
Luis F. Tello A. <seizin00@tuta.io>
2026-04-16 22:47:28 -0500

    dummy data w/third-packages and get data

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'my_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- my_blog/post/views.py
+++ my_blog/post/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Author
 
 
+def queries(request):
+  authors = Author.objects.all()
+
+  filtered = Author.objects.filter(email='janegonzalez@example.org')
+
+  author = Author.objects.get(id=1)
+
+  offsets = Author.objects.all()[5:10]
+
+  return render(request, 'post/queries.html', {'authors': authors, 'filtered': filtered, 'author': author, 'offsets': offsets})
